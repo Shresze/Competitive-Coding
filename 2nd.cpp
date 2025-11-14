e5ce0f2295f2d51979d7d0396f2d0ae1445601d4
Shresze <shreshta0611@gmail.com>
2025-11-14 12:18:22 +0530

    Create 2nd.cpp

---
+++ 2nd.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        for(int i=0; i<k; i++) {
+            int e=nums[i];
+            while(!dq.empty() && nums[dq.back()]<e) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+
+        vector<int> a;
+        a.push_back(nums[dq.front()]);
+
+        for(int i=k; i<nums.size(); i++) {
+            if(!dq.empty() && dq.front()<=i-k) {
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[dq.back()]<nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            a.push_back(nums[dq.front()]);
+        }
+        
+        return a;
+    }
+};
+   
